f3cb33db2b423c757b395f831e3c21805a2940fc
LausYr <73410477+LausYr@users.noreply.github.com>
2021-10-27 18:12:37 +0500

    Create ApplicationContext.cs

---
+++ Directory.WebAPI/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Directory.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Directory.WebAPI.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
