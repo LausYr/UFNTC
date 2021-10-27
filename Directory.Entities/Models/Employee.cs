32f3689ba45ea1dbfc3e91792a2662d35ab3cce8
LausYr <73410477+LausYr@users.noreply.github.com>
2021-10-27 18:11:55 +0500

    Создание моделей
    Employee
    Organization

---
+++ Directory.Entities/Models/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.Entities.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Используйте кириллицу")]
+        [MaxLength(256, ErrorMessage = "Длина поля LastName 256 символов")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Используйте кириллицу")]
+        [MaxLength(256, ErrorMessage = "Длина поля FirstName 256 символов")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [RegularExpression(@"^[а-яА-ЯёЁ]+$", ErrorMessage = "Используйте кириллицу")]
+        [MaxLength(256, ErrorMessage = "Длина поля Patronymic 256 символов")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Введите номер телефона")]
+        [RegularExpression(@"^((7|8)+([0-9]){10})$", ErrorMessage = "Неверный формат номера телефона")]
+        [MaxLength(11, ErrorMessage = "Длина поля PhoneNumber 11 символов")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Введите Email")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        [MaxLength(256, ErrorMessage = "Длина поля Email 256 символов")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите подразделение")]
+        [MaxLength(256, ErrorMessage = "Длина поля Subdivision  256 символов")]
+        public string Subdivision { get; set; }
+
+        [Required(ErrorMessage = "Введите должность")]
+        [MaxLength(256, ErrorMessage = "Длина поля PositionWork  256 символов")]
+        public string PositionWork { get; set; }
+
+        public Guid OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+       
+
+    }
+}
---
+++ Directory.Entities/Models/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory.Entities.Models
+{
+    public class Organization
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Введите название организации")]
+        [MaxLength(256, ErrorMessage = "Длина поля Email 256 символов")]
+        public string Name { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
